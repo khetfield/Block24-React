73c851c0fad0da36d8b5a6c7850eeaefe970f07d
khetfield <163695693+khetfield@users.noreply.github.com>
2024-05-07 19:50:55 -0400

    submit
    submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { puppyList } from './data.js';
 
-      <div>
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
-      </div>
-
-
-export default App
+  return (
+    <div className="container">
+      {puppies.map((p) => {
+        return (
+          <p
+            key={p.id}
+            onClick={() => {
+              setFeatPupId(p.id);
+            }}
+            className="name-box"
+          >
+            {p.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div id="feat">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+export default App;
